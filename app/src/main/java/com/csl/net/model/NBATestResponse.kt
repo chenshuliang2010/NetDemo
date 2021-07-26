0243c2da3510d8a9e3d445c2b6d990b0608bc42d
86135 <chenshuliang2010@sina.com>
2021-07-26 15:16:44 +0800

    新增有效网络请求接口

---
+++ app/src/main/java/com/csl/net/model/NBATestResponse.kt
@@ -0,0 +1,15 @@
+package com.csl.net.model
+
+/**
+ *Created by 86135
+ *on 2021/7/26
+ */
+class NBATestResponse(
+    val error_coed: Int,
+    val reason: String,
+    val result: Any?,
+    val title: String,
+    val duration: String,
+    val date: Any?,
+    val week: String
+)
